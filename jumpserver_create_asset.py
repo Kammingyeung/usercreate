a83ea927cb48a0dfea0263aecf7abae424b85fac
Kamminggo <13266315863@139.com>
2024-06-04 16:06:38 +0800

    页面创建

---
+++ jumpserver_create_asset.py
@@ -0,0 +1,50 @@
+import requests
+import datetime
+import json
+from httpsig.requests_auth import HTTPSignatureAuth
+
+
+def get_auth(KeyID, SecretID):
+    signature_headers = ['(request-target)', 'accept', 'date']
+    auth = HTTPSignatureAuth(key_id=KeyID, secret=SecretID, algorithm='hmac-sha256', headers=signature_headers)
+    return auth
+
+
+def create_asset_permission(jms_url, auth, name, user_uuid):
+    url = jms_url + '/api/v1/perms/asset-permissions/'
+    gmt_form = '%a, %d %b %Y %H:%M:%S GMT'
+    headers = {
+        'Accept': 'application/json',
+        'X-JMS-ORG': '00000000-0000-0000-0000-000000000002',
+        'Date': datetime.datetime.utcnow().strftime(gmt_form),
+        'Content-Type': 'application/json'  # 添加Content-Type头部
+    }
+
+    permission_data = {
+        "name": name,
+        "users": [user_uuid]
+    }
+
+    # 发送POST请求
+    response = requests.post(url, auth=auth, headers=headers, json=permission_data)
+    print(json.loads(response.text))
+
+
+if __name__ == '__main__':
+    jms_url = 'http://172.16.22.41'
+    KeyID = 'dc7cad72-27e4-4151-b78d-497e191ab5ba'
+    SecretID = 'EAw4KR81v4hrhrvXliv8jF403ClG6MYREgNF'
+    # jms_url = 'http://172.31.15.114:36180'
+    # KeyID = '5102eddf-6072-459c-b48a-bdffef372453'
+    # SecretID = '9dbf7dbb-4a3f-478d-a3e2-3ce6bb15df53'
+    auth = get_auth(KeyID, SecretID)
+
+    # 创建资产授权的名称和用户 UUID 列表
+    permissions_list = [
+        ("tt1", "dcc3608b-4941-442a-bced-2d569521380f"),
+        ("tt2", "dcc3608b-4941-442a-bced-2d569521380s"),
+    ]
+
+    # 循环创建资产授权
+    for name, user_uuid in permissions_list:
+        create_asset_permission(jms_url, auth, name, user_uuid)
--- jumpserver_get.py
+++ jumpserver_get.py
@@ -9,7 +9,8 @@ def get_auth(KeyID, SecretID):
     return auth
 
 def get_user_info(jms_url, auth):
-    url = jms_url + '/api/v1/users/users/'
+    # url = jms_url + '/api/v1/users/users/'
+    url = jms_url + '/api/v1/perms/asset-permissions/'
     gmt_form = '%a, %d %b %Y %H:%M:%S GMT'
     headers = {
         'Accept': 'application/json',
@@ -21,8 +22,11 @@ def get_user_info(jms_url, auth):
     print(json.loads(response.text))
 
 if __name__ == '__main__':
-    jms_url = 'http://172.16.22.41'
-    KeyID = '74a76830-9601-4bb1-9669-7f84cd8eab4f'
-    SecretID = '8bbnPuH5TGJbredhPfzNycmX772qf6rVJl45'
+    # jms_url = 'http://172.16.22.41'
+    # KeyID = 'dc7cad72-27e4-4151-b78d-497e191ab5ba'
+    # SecretID = 'EAw4KR81v4hrhrvXliv8jF403ClG6MYREgNF'
+    jms_url = 'http://172.31.15.114:36180'
+    KeyID = '5102eddf-6072-459c-b48a-bdffef372453'
+    SecretID = '9dbf7dbb-4a3f-478d-a3e2-3ce6bb15df53'
     auth = get_auth(KeyID, SecretID)
     get_user_info(jms_url, auth)
